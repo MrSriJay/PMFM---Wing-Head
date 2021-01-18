f5926273a2185d5b4ac6a127662001699f7a3235
Jayanga Palihena <jayanga.sl@gmail.com>
2021-01-18 10:05:57 +0530

    ss

--- resources/views/layouts/AdminMaster.blade.php
+++ resources/views/layouts/AdminMaster.blade.php
@@ -88,7 +88,7 @@
       <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
         <div class="container-fluid">
         <div class="navbar-wrapper">
-            <a class="navbar-brand" href="javascript:;">Admin View</a>
+          <a class="navbar-brand" href="javascript:;"><i class="material-icons">admin_panel_settings</i> Admin View</a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
