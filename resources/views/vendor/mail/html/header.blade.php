f6daa7bb18de472c995dc2bd362483dbd9b270f0
Jayanga Palihena <jayanga.sl@gmail.com>
2021-05-29 15:33:48 +0530

    git

--- resources/views/vendor/mail/html/header.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    <img src="https://i.postimg.cc/PrbNz1Xv/New-LOGO.png" width="150px" alt="crdlogo"/></a>
-</a>
-</td>
-</tr>
