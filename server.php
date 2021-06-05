fa0e2e03b7b95848dcb80169ef9200eabe5ad6c8
Jayanga Palihena <jayanga.sl@gmail.com>
2021-06-05 18:52:43 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,73 +1,21 @@
 <?php
 
-namespace React\Socket;
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-use Evenement\EventEmitter;
-use React\EventLoop\LoopInterface;
-use Exception;
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-final class Server extends EventEmitter implements ServerInterface
-{
-    private $server;
-
-    public function __construct($uri, LoopInterface $loop, array $context = array())
-    {
-        // sanitize TCP context options if not properly wrapped
-        if ($context && (!isset($context['tcp']) && !isset($context['tls']) && !isset($context['unix']))) {
-            $context = array('tcp' => $context);
-        }
-
-        // apply default options if not explicitly given
-        $context += array(
-            'tcp' => array(),
-            'tls' => array(),
-            'unix' => array()
-        );
-
-        $scheme = 'tcp';
-        $pos = \strpos($uri, '://');
-        if ($pos !== false) {
-            $scheme = \substr($uri, 0, $pos);
-        }
-
-        if ($scheme === 'unix') {
-            $server = new UnixServer($uri, $loop, $context['unix']);
-        } else {
-            $server = new TcpServer(str_replace('tls://', '', $uri), $loop, $context['tcp']);
-
-            if ($scheme === 'tls') {
-                $server = new SecureServer($server, $loop, $context['tls']);
-            }
-        }
-
-        $this->server = $server;
-
-        $that = $this;
-        $server->on('connection', function (ConnectionInterface $conn) use ($that) {
-            $that->emit('connection', array($conn));
-        });
-        $server->on('error', function (Exception $error) use ($that) {
-            $that->emit('error', array($error));
-        });
-    }
-
-    public function getAddress()
-    {
-        return $this->server->getAddress();
-    }
-
-    public function pause()
-    {
-        $this->server->pause();
-    }
-
-    public function resume()
-    {
-        $this->server->resume();
-    }
-
-    public function close()
-    {
-        $this->server->close();
-    }
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
 }
+
+require_once __DIR__.'/public/index.php';
